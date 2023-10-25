08c8da4ae1d75e4771a334a240c3e95a7e9c8d7d
jiseung <dickseung23@naver.com>
2023-10-25 21:31:06 +0900

    # API 리턴타입 리팩토링

--- src/main/java/kiosk/pleaKiosk/domain/controller/ProductController.java
+++ src/main/java/kiosk/pleaKiosk/domain/controller/ProductController.java
@@ -1,20 +1,18 @@
 package kiosk.pleaKiosk.domain.controller;
-import kiosk.pleaKiosk.domain.dto.request.PageRequeust;
+import kiosk.pleaKiosk.domain.codes.ErrorCode;
 import kiosk.pleaKiosk.domain.dto.request.ProductModifyRequest;
 import kiosk.pleaKiosk.domain.dto.request.ProductSaveRequest;
+import kiosk.pleaKiosk.domain.dto.response.ApiResponse;
 import kiosk.pleaKiosk.domain.entity.Product;
-import kiosk.pleaKiosk.domain.exception.CustomException;
-import kiosk.pleaKiosk.domain.exception.ErrorCode;
 import kiosk.pleaKiosk.domain.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -23,40 +21,33 @@ public class ProductController {
 
     private final ProductService productService;
 
-
     @PostMapping("/register")
-    public ResponseEntity<String> saveProduct(@Valid @RequestBody ProductSaveRequest productSaveRequest) {
-
-            productService.registerProduct(productSaveRequest);
-            return ResponseEntity.ok().body("상품저장완료");
-
+    public ApiResponse<Product> saveProduct(@Valid @RequestBody ProductSaveRequest productSaveRequest) {
+        return productService.registerProduct(productSaveRequest);
     }
 
     @GetMapping("/getall-product")
-    public Page<Product> allProducts(@RequestParam(defaultValue = "1") int page,
-                                     @RequestParam(defaultValue = "10") int size,
-                                     @RequestParam(defaultValue = "id") String sort){
+    public ApiResponse<Page<Product>> allProducts(@RequestParam(defaultValue = "1") int page,
+                                                  @RequestParam(defaultValue = "10") int size,
+                                                  @RequestParam(defaultValue = "id") String sort) throws IOException {
         if (page < 1 || size < 1) {
-            throw new CustomException(ErrorCode.BAD_REQUEST_PAGE);
+            throw new IOException(String.valueOf(ErrorCode.IO_ERROR));
         }
         Pageable pageable = PageRequest.of(page - 1, size, Sort.by(sort));
         return productService.getAllProduct(pageable);
     }
 
     @RequestMapping(value = "/modify",method = {RequestMethod.PUT ,RequestMethod.PATCH})
-    public ResponseEntity<String> modifyProduct(@Valid @RequestBody ProductModifyRequest productModifyRequest){
+    public ApiResponse<Product> modifyProduct(@Valid @RequestBody ProductModifyRequest productModifyRequest){
 
-        productService.modifyProduct(productModifyRequest);
+        return productService.modifyProduct(productModifyRequest);
 
-        return ResponseEntity.ok().body(productModifyRequest.getId() + "번 상품 정보 수정완료");
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+    public ApiResponse deleteProduct(@PathVariable Long id){
 
-        productService.deleteProduct(id);
-
-        return ResponseEntity.ok().body(id + "번 상품 삭제완료");
+        return productService.deleteProduct(id);
     }
 
 
--- src/main/java/kiosk/pleaKiosk/domain/service/ProductService.java
+++ src/main/java/kiosk/pleaKiosk/domain/service/ProductService.java
@@ -1,10 +1,11 @@
 package kiosk.pleaKiosk.domain.service;
+import kiosk.pleaKiosk.domain.codes.ErrorCode;
+import kiosk.pleaKiosk.domain.codes.SuccessCode;
 import kiosk.pleaKiosk.domain.dto.request.ProductModifyRequest;
 import kiosk.pleaKiosk.domain.dto.request.ProductSaveRequest;
+import kiosk.pleaKiosk.domain.dto.response.ApiResponse;
 import kiosk.pleaKiosk.domain.entity.Order;
 import kiosk.pleaKiosk.domain.entity.Product;
-import kiosk.pleaKiosk.domain.exception.CustomException;
-import kiosk.pleaKiosk.domain.exception.ErrorCode;
 import kiosk.pleaKiosk.domain.repository.OrderRepository;
 import kiosk.pleaKiosk.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -27,7 +28,7 @@ public class ProductService {
     private final OrderRepository orderRepository;
 
     @Transactional
-    public void registerProduct(ProductSaveRequest productSaveRequest) {
+    public ApiResponse<Product> registerProduct(ProductSaveRequest productSaveRequest) {
         log.info("상품 등록 로직 시작 - productSaveRequest = {}",productSaveRequest);
 
         Product saveProduct = Product
@@ -36,48 +37,54 @@ public class ProductService {
                 .price(productSaveRequest.getPrice())
                 .amount(productSaveRequest.getAmount())
                 .build();
-        productRepository.save(saveProduct);
+        Product savedProduct = productRepository.save(saveProduct);
         log.info("상품 등록 로직 종료");
+
+        return new ApiResponse(savedProduct,SuccessCode.INSERT_SUCCESS.getStatus(),SuccessCode.INSERT_SUCCESS.getMessage());
     }
 
     @Transactional(readOnly = true)
-    public Page<Product> getAllProduct(Pageable pageable) {
+    public ApiResponse<Page<Product>> getAllProduct(Pageable pageable) {
         log.info("모든상품 조회 로직 시작 = {}",pageable);
         Page<Product> allProduct = productRepository.findAll(pageable);
         log.info("모든상품 조회 로직 종료 = {}",allProduct.getSize());
-        return allProduct;
+        return new ApiResponse(allProduct,SuccessCode.SELECT_SUCCESS.getStatus(), SuccessCode.SELECT_SUCCESS.getMessage());
     }
 
     @Transactional
-    public void modifyProduct(ProductModifyRequest productModifyRequest) {
+    public ApiResponse<Product> modifyProduct(ProductModifyRequest productModifyRequest) {
         log.info("상품 정보 수정 로직 시작 productModifyRequest = {}",productModifyRequest);
 
-        Product findProductById = productRepository.findById(productModifyRequest.getId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ITEM));
+        Product findProductById = productRepository.findById(productModifyRequest.getId()).orElseThrow(() -> new RuntimeException(String.valueOf(ErrorCode.UPDATE_ERROR)));
 
         findOrderListProduct(findProductById);
 
         findProductById.updateEntity(productModifyRequest.getName(),productModifyRequest.getPrice(),productModifyRequest.getAmount());
         log.info("상품 정보 수정 로직 종료 productModifyRequest = {}",findProductById);
 
+        return new ApiResponse(findProductById,SuccessCode.UPDATE_SUCCESS.getStatus(), SuccessCode.UPDATE_SUCCESS.getMessage());
+
     }
 
     private void findOrderListProduct(Product findProductById) {
         List<Order> findOrderListByProduct= orderRepository.findByProduct(findProductById);
         if(!findOrderListByProduct.isEmpty()){
-            throw new CustomException(ErrorCode.CONFLICT);
+            throw new RuntimeException(String.valueOf(ErrorCode.UPDATE_ERROR));
         }
     }
 
     @Transactional
-    public void deleteProduct(Long id) {
+    public ApiResponse deleteProduct(Long id) {
         log.info("상품삭제 로직 시작  - {}",id);
 
-        Product findProductById = productRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ITEM));
+        Product findProductById = productRepository.findById(id).orElseThrow(() -> new RuntimeException(String.valueOf(ErrorCode.UPDATE_ERROR)));
 
         findOrderListProduct(findProductById);
 
         productRepository.deleteById(id);
 
         log.info("상품삭제 로직 종료");
+
+        return new ApiResponse(SuccessCode.DELETE_SUCCESS.getStatus(), SuccessCode.DELETE_SUCCESS.getMessage());
     }
 }
