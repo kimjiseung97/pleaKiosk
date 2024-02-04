c8a3c2c60f3c2d643e0d78098162d9d56b905e37
jiseung <dickseung23@naver.com>
2024-02-04 15:31:57 +0900

    feat [main] github action 테스트

--- src/main/java/kiosk/pleaKiosk/domain/repository/Order1HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package kiosk.pleaKiosk.domain.repository;
-
-import kiosk.pleaKiosk.domain.entity.Order1History;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Order1HistoryRepository extends JpaRepository<Order1History,Long> {
-}
